0b21e2ec74cedd472ea8567b570ed65ae9a26555
Badi Dinesh Reddy <148687590+dinesh0118@users.noreply.github.com>
2024-02-21 23:49:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed= new Typed(".text",{
+    strings:["Frontend Developer" , "Backend Developer" , "Full Stack Developer"],
+    typeSpeed:80,
+    backSpeed:80,
+    backDelay:1000,
+    loop:true,
+    showCursor:false
+});
